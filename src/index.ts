3d08f1e8d3b83b5fb7e478caafeddf69adaf4c85
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-08 10:21:37 -0300

    :package: Organização inicial de diretórios

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-import express, {Express} from 'express'
-import cors from 'cors'
-import { AddressInfo } from "net";
-import knex from "knex";
-import dotenv from "dotenv";
+// import express, {Express} from 'express'
+// import cors from 'cors'
+// import { AddressInfo } from "net";
+// import knex from "knex";
+// import dotenv from "dotenv";
 
-dotenv.config();
-const app: Express = express();
+// dotenv.config();
 
-app.use(express.json());
-app.use(cors());
+import { app } from "./app";
+
+// const app: Express = express();
+
+// app.use(express.json());
+// app.use(cors());
 
 
 
-const server = app.listen(process.env.PORT || 3003, () => {
-    if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
-    } else {
-       console.error(`Failure upon starting server.`);
-    }
-});
+// const server = app.listen(process.env.PORT || 3003, () => {
+//     if (server) {
+//        const address = server.address() as AddressInfo;
+//        console.log(`Server is running in http://localhost: ${address.port}`);
+//     } else {
+//        console.error(`Failure upon starting server.`);
+//     }
+// });
