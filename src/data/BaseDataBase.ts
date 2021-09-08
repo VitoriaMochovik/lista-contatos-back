f74c0bafe14a4de59099ba61cc70821d7cef7205
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-08 10:41:41 -0300

    :arrow_up: Criação da tabela de contatos

---
+++ src/data/BaseDataBase.ts
@@ -0,0 +1,41 @@
+import knex from "knex"
+import { config } from "dotenv"
+
+config()
+
+export class BaseDataBase {
+
+    protected static connection = knex({
+        client: "mysql",
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_SCHEMA,
+            port: 3306,
+            multiStatements: true
+        }
+    })
+
+
+    private printError = (error: any) => {
+        console.log(error.sqlMessage || error.message) 
+    }
+
+    createTables = () => BaseDataBase
+        .connection
+        .raw(`
+
+            CREATE TABLE IF NOT EXISTS contact_cist_users (
+                id VARCHAR(255) PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                telephone_number VARCHAR(20) NOT NULL
+            );
+        
+        `)
+        .then(() => {
+            console.log("Table successfully created!")
+        })
+        .catch(this.printError)
+}
