31b0b5bcf75a6a735fed25b6e18c38874f5ed8d4
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-09 15:43:57 -0300

    Alteração teste

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,13 @@
 import { app } from "./app";
-
-
-
-import { app } from "./app";
 import { getAllUsers } from "./endpoints/users/getAllUsers";
-
 import { deleteUser } from "./endpoints/users/deleteUser";
-
-
 import { updateUser } from "./endpoints/users/updateUser";
-
-
-app.put("/contacts/:id", updateUser)
-
-
-// const app: Express = express();
-
-// app.use(express.json());
-// app.use(cors());
-
-
-
-app.get("/contacts", getAllUsers)
-// const server = app.listen(process.env.PORT || 3003, () => {
-//     if (server) {
-//        const address = server.address() as AddressInfo;
-//        console.log(`Server is running in http://localhost: ${address.port}`);
-//     } else {
-//        console.error(`Failure upon starting server.`);
-//     }
-// });
-
-app.delete("/contacts/:id", deleteUser)
-
-
 import { createUser } from "./endpoints/users/createUser";
 
 
-
+app.put("/contacts/:id", updateUser)
+app.get("/contacts", getAllUsers)
+app.delete("/contacts/:id", deleteUser)
 app.post("/contats", createUser)
 
 
