6bddef11845414cd170633609d1d9ea7f7c04399
Vitória Carolina Mochovik <mochovik.vc@gmail.com>
2021-09-10 01:02:18 -0300

    :arrow_up: criação de endpoint de pegar contato pelo usuario

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import { getAllUsers } from "./endpoints/users/getAllUsers";
 import { deleteUser } from "./endpoints/users/deleteUser";
 import { updateUser } from "./endpoints/users/updateUser";
 import { createUser } from "./endpoints/users/createUser";
+import { getUserById } from "./endpoints/users/getUserById";
+
 
 
 app.put("/contacts/:id", updateUser)
 app.get("/contacts", getAllUsers)
+app.get("/contacts/:id", getUserById)
 app.delete("/contacts/:id", deleteUser)
 app.post("/contacts", createUser)
 
